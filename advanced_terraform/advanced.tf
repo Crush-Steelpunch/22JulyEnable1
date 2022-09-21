17afa0f303f727aac49268bbf51832db1ddfd1ca
Leon L. Robinson <leon.robinson@qa.com>
2022-09-21 13:11:31 +0100

    advanced terraform

---
+++ advanced_terraform/advanced.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "tf-instance" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.inst_type}"
+  key_name      = "22Julyenable1-Lon"
+  security_groups = ["AutoScaling-Security-Group-1"]
+  root_block_device { volume_size = "${var.eph_block_size}"}
+  
+  }
